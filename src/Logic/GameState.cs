7328969a9d370e50bee1c9c1d8f833e64721e0eb
lukas027 <lukas.weiss27@web.de>
2014-08-04 17:08:56 +0200

    Logic classes

---
+++ src/Logic/GameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D2048.Logic
+{
+    class GameState
+    {
+        public int [, ,] field = new int[4, 4, 4];
+    }
+}
