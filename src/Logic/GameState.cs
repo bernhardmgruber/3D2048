aad6c9b5441f6d9b77e898da805d80c80ce465e1
lukas027 <lukas.weiss27@web.de>
2014-08-05 12:33:33 +0200

    Finished GameLogic

--- src/Logic/GameState.cs
+++ src/Logic/GameState.cs
@@ -9,11 +9,8 @@ namespace _3D2048.Logic
     class GameState
     {
         public int [, ,] field = new int[4, 4, 4];
+        public bool lost = false;
 
-        public int getNumber(int x, int y, int z)
-        {
-            return field[x, y, z];
-        }
-
+     
     }
 }
