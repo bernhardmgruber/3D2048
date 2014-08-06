a675c106f03c26e2c0175265ef79df7ef9f61330
lukas027 <lukas.weiss27@web.de>
2014-08-06 18:15:43 +0200

    Refactored logic, seems to work now

--- src/Logic/GameLogic.cs
+++ src/Logic/GameLogic.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using _3D2048.Util;
+using System.Diagnostics;
 
 namespace _3D2048.Logic
 {
@@ -14,8 +15,6 @@ namespace _3D2048.Logic
         public GameLogic()
         {
             gameModel = new GameState();
-           
-
         }
 
         public Direction getMoveDependentDirection(Direction direction, Camera gameCamera)
@@ -105,163 +104,78 @@ namespace _3D2048.Logic
             return outputDirection;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="axis">The axis along to collapse. Must be 0, 1 or 2 (for x, y, and z)</param>
+        /// <param name="stepDir">Either +1 or -1 for the direction in which to collapse</param>
+        private void collapse(int axis, int stepDir) 
+        {
+            int[] pos = new int[3];
+            int[] nextPos = new int[3];
+
+            // enumerate all stacks to collapse
+            for (int i = 0; i < GameState.size; i++)
+            {
+                for (int j = 0; j < GameState.size; j++)
+                {
+                    bool modified;
+                    do
+                    {
+                        modified = false;
+
+                        pos[axis] = stepDir > 0 ? GameState.size - 1 : 0;
+                        pos[(axis + 1) % 3] = i;
+                        pos[(axis + 2) % 3] = j;
+
+                        // for each stack element
+                        for (int l = 0; l < GameState.size - 1; l++)
+                        {
+                            Array.Copy(pos, nextPos, 3);
+                            nextPos[axis] -= stepDir;
+
+                            if (gameModel.field[pos[0], pos[1], pos[2]] == 0)
+                            {
+                                if(gameModel.field[nextPos[0], nextPos[1], nextPos[2]] != 0)
+                                {
+                                    // move number from next to pos
+                                    gameModel.field[pos[0], pos[1], pos[2]] = gameModel.field[nextPos[0], nextPos[1], nextPos[2]];
+                                    gameModel.field[nextPos[0], nextPos[1], nextPos[2]] = 0;
+                                    modified = true;
+                                }
+                            }
+                            else if (gameModel.field[pos[0], pos[1], pos[2]] == gameModel.field[nextPos[0], nextPos[1], nextPos[2]]) // previous condition ensures that pos and nextPos are not 0
+                            {
+                                Debug.Assert(gameModel.field[pos[0], pos[1], pos[2]] != 0);
+
+                                // combine numbers into pos
+                                gameModel.field[pos[0], pos[1], pos[2]] *= 2;
+                                gameModel.field[nextPos[0], nextPos[1], nextPos[2]] = 0;
+                                modified = true;
+                            }
+
+                            Array.Copy(nextPos, pos, 3);
+                        }
+                    }
+                    while (modified);
+                }
+            }
+        }
+
         public void Move(Direction direction)
         {
+            // collapse current numbers
             switch (direction)
             {
-                case Logic.Direction.Right:
-
-                    for (int i = 0; i < GameState.size - 1; i++)
-                    {
-                        for (int j = 0; j < GameState.size; j++)
-                        {
-                            for (int l = 0; l < GameState.size; l++)
-                            {
-                              if (gameModel.field[i+1, j, l] == 0)
-                                {
-                                    gameModel.field[i + 1, j, l] = gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-                                else if (gameModel.field[i, j, l] == gameModel.field[i + 1, j, l])
-                                {
-                                    gameModel.field[i + 1, j, l] = gameModel.field[i + 1, j, l] + gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-                            }
-                        }
-                    }
-
-                    break;
-
-                case Logic.Direction.Left:
-
-                    for (int i = GameState.size - 1; i > 0; i--)
-                    {
-                        for (int j = 0; j < GameState.size; j++)
-                        {
-                            for (int l = 0; l < GameState.size; l++)
-                            {
-                              if (gameModel.field[i-1, j, l] == 0)
-                                {
-                                    gameModel.field[i - 1, j, l] = gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-                                else if (gameModel.field[i, j, l] == gameModel.field[i - 1, j, l])
-                                {
-                                    gameModel.field[i - 1, j, l] = gameModel.field[i - 1, j, l] + gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-                            }
-                        }
-                    }
-
-                    break;
-
-                case Logic.Direction.Up:
-
-                    for (int j = 0; j < GameState.size - 1; j++)
-                    {
-                        for (int i = 0; i < GameState.size; i++)
-                        {
-                            for (int l = 0; l < GameState.size; l++)
-                            {
-                              if (gameModel.field[i, j+1, l] == 0)
-                                {
-                                    gameModel.field[i , j+1, l] = gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-                                else if (gameModel.field[i, j, l] == gameModel.field[i, j+1, l])
-                                {
-                                    gameModel.field[i, j + 1, l] = gameModel.field[i, j + 1, l] + gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-                            }
-                        }
-                    }
-
-                    break;
-
-                case Logic.Direction.Down:
-
-                    for (int j = GameState.size - 1; j > 0; j--)
-                    {
-                        for (int i = 0; i < GameState.size; i++)
-                        {
-                            for (int l = 0; l < GameState.size; l++)
-                            {
-                              if (gameModel.field[i, j-1, l] == 0)
-                                {
-                                    gameModel.field[i , j-1, l] = gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-                                else if (gameModel.field[i, j, l] == gameModel.field[i, j-1, l])
-                                {
-                                    gameModel.field[i, j-1, l] = gameModel.field[i, j-1, l] + gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-                            }
-                        }
-                    }
-
-                    break;
-
-                case Logic.Direction.Back:
-
-                    for (int l = GameState.size - 1; l > 0; l--)
-                    {
-                        for (int j = 0; j < GameState.size; j++)
-                        {
-                            for (int i = 0; i < GameState.size; i++)
-                            {
-                                if (gameModel.field[i, j, l-1] == 0)
-                                {
-                                    gameModel.field[i, j, l-1] = gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-                                else if (gameModel.field[i, j, l] == gameModel.field[i, j, l - 1])
-                                {
-                                    gameModel.field[i, j, l - 1] = gameModel.field[i, j, l - 1] + gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-
-                            }
-                        }
-                    }
-
-                    break;
-                case Logic.Direction.Forward:
-
-                    for (int l = 0; l < GameState.size - 1; l++)
-                    {
-                        for (int j = 0; j < GameState.size; j++)
-                        {
-                            for (int i = 0; i < GameState.size; i++)
-                            {
-                                if (gameModel.field[i, j, l+1] == 0)
-                                {
-                                    gameModel.field[i, j, l + 1] = gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-                                else if (gameModel.field[i, j, l] == gameModel.field[i, j, l + 1])
-                                {
-                                    gameModel.field[i, j, l + 1] = gameModel.field[i, j, l + 1] + gameModel.field[i, j, l];
-                                    gameModel.field[i, j, l] = 0;
-                                }
-
-
-                            }
-                        }
-                    }
-                    break;
+                case Logic.Direction.Right:   collapse(0, +1); break;
+                case Logic.Direction.Left:    collapse(0, -1); break;
+                case Logic.Direction.Up:      collapse(1, +1); break;
+                case Logic.Direction.Down:    collapse(1, -1); break;
+                case Logic.Direction.Back:    collapse(2, -1); break;
+                case Logic.Direction.Forward: collapse(2, +1); break;
             }
 
+            // randomly insert a new 2
             bool freeField = false;
 
             for (int l = 0; l < GameState.size; l++)
@@ -331,9 +245,6 @@ namespace _3D2048.Logic
                 }
             }
 
-
-     
-             
         }
     }
 }
