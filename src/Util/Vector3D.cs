b248971de1b843587b8f967e1436168dfe36c415
Dominik Stolz <crazymrcat@gmail.com>
2014-08-06 12:18:09 +0200

    added length to vector3D

--- src/Util/Vector3D.cs
+++ src/Util/Vector3D.cs
@@ -12,6 +12,15 @@ namespace _3D2048.Util
         public float y;
         public float z;
 
+        public float Length
+        {
+            public get
+            {
+                return (float) Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2))
+            }
+            private set;
+        }
+
         public Vector3D(float x, float y, float z)
         {
             this.x = x;
@@ -26,9 +35,15 @@ namespace _3D2048.Util
             this.z += z;
         }
 
+
         public static Vector3D operator+(Vector3D vec1, Vector3D vec2)
         {
             return new Vector3D(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
+        }
+
+        public static Vector3D operator -(Vector3D vec1, Vector3D vec2)
+        {
+            return new Vector3D(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
         }
 
     }
