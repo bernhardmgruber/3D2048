08e4e86717832c59c3790f93be12e1c3c570cc03
htinc <yo.ey.abc@gmail.com>
2014-08-07 12:03:41 +0200

    Better texture numbers

--- src/Rendering/Textures.cs
+++ src/Rendering/Textures.cs
@@ -50,15 +50,9 @@ namespace _3D2048.Rendering
             g.SmoothingMode = SmoothingMode.AntiAlias;
             g.InterpolationMode = InterpolationMode.HighQualityBicubic;
             g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-            g.DrawString(number.ToString(), new Font("Tahoma", (int)(0.2 * b.Width)), Brushes.Black, rect, sf);
-
+            g.DrawString(number.ToString(), new Font("Courier New", (int)(0.3 * b.Width)), Brushes.Black, rect, sf);
             g.Flush();
-
-
-
-
             Texture t = new Texture();
-
             t.Create(this.gl, b);
             return t;
         }
