9653d7f1befb5c76ed89f2b5c04a2482c2469266
Julian Sperling <juliansperling@gmail.com>
2014-08-07 16:54:16 +0200

    Fixed texture face bug and started adding commentations

--- src/Rendering/Renderer.cs
+++ src/Rendering/Renderer.cs
@@ -13,16 +13,21 @@ namespace _3D2048.Rendering
 {
     class Renderer
     {
+        //Declaration of necessary objects
         private OpenGL gl;
-        private Textures textures;
+        private Textures textures;      
 
 
         public Renderer(OpenGL glIn)
         {
+            //Generates Textures for input OpenGL Object
             gl = glIn;
             textures = new Textures(gl);
 
+
             gl.Enable(OpenGL.GL_TEXTURE_2D);
+            gl.Enable(OpenGL.GL_CULL_FACE );
+            //Removal of all unwanted colours
             gl.ClearColor(0.0f, 0.0f, 0.2f, 1.0f);
         }
 
@@ -31,20 +36,23 @@ namespace _3D2048.Rendering
         public void draw(Camera camera, GameState state)
         {
 
-
+            //Setup of the gl Object in conjunction with camera Settings
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
             gl.Enable(OpenGL.GL_BLEND);
             gl.BlendFunc(OpenGL.GL_SRC_ALPHA, OpenGL.GL_ONE_MINUS_SRC_ALPHA);
             gl.LoadIdentity();
             gl.Translate(0.0f, 0.0f, camera.zoom);
             gl.Rotate(camera.cubeRotation.x, camera.cubeRotation.y, camera.cubeRotation.z);
-            gl.Color(1.0f, 1.0f, 1.0f, 0.45f);
+            gl.Color(1.0f, 1.0f, 1.0f, 0.45f); //Base Color with alpha(transparency) value
 
-            float[] mv = new float[16];
+            //Gettting of the OpenGL Modelview Matrix Parameters
+            float[] mv = new float[16]; 
             gl.GetFloat(OpenGL.GL_MODELVIEW_MATRIX, mv);
 
+            //Setup of a new List of Tuples(for multiple Parameters) to store Cube information
             List<Tuple<Vector3D, int, float>> values = new List<Tuple<Vector3D, int, float>>();
 
+            //Runthrough of the Gamestate Matrix in search for cubes to be rendered
             for (int i = 0; i < GameState.size; i++)
             {
                 for (int j = 0; j < GameState.size; j++)
@@ -53,9 +61,9 @@ namespace _3D2048.Rendering
                     {
                         if (state.field[i, j, k] != 0)
                         {
-                            Vector3D cubeData = new Vector3D(-GameState.size / 2.0f + 0.5f + i, -GameState.size / 2.0f + 0.5f + j, -GameState.size / 2.0f + 0.5f + k);
-                            int cubeValue = state.field[i, j, k];
-                            float depth = cubeData*new Vector3D(mv[2],mv[6],mv[10]);
+                            Vector3D cubeData = new Vector3D(-GameState.size / 2.0f + 0.5f + i, -GameState.size / 2.0f + 0.5f + j, -GameState.size / 2.0f + 0.5f + k); //Calculates and saves a single cubes object coord center on the world coordinates  
+                            int cubeValue = state.field[i, j, k]; //Saves Value of a Cube from the current Position in the gamestate Matrix
+                            float depth = cubeData*new Vector3D(mv[2],mv[6],mv[10]); //
                             values.Add(new Tuple<Vector3D, int, float>(cubeData, cubeValue, depth));
                         }
                     }
