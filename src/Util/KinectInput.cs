68b5a1cad18a803296833f6062578a321f40b0d6
MrRonny6 <emil.sage@gmx.de>
2014-08-07 18:03:41 +0200

    Complete Kinect Input

--- src/Util/KinectInput.cs
+++ src/Util/KinectInput.cs
@@ -24,6 +24,7 @@ namespace _3D2048.Util
         private bool moveDown = false;
         private bool moveForward = false;
         private bool moveBack = false;
+        private bool moveTriggered = false;
 
         public KinectInput(GameLogic logic, Camera cam)
         {
@@ -91,8 +92,58 @@ namespace _3D2048.Util
                 Vector3D rightShoulder = convertSkeletonPoint(workingSkeleton.Joints[JointType.ShoulderRight].Position);
                 float triggerDistance = (rightShoulder - convertSkeletonPoint(workingSkeleton.Joints[JointType.ShoulderLeft].Position)).Length;
                 Vector3D handShoulderVector = rightHand - rightShoulder;
+                
                 if (watchMovement)
                 {
+
+                    if (rightHand.x > rightShoulder.x + (triggerDistance / 2) && !moveTriggered) // Right movement
+                    {
+                        Console.WriteLine("EXEC right");
+                        logic.Move(Direction.Right);
+                        moveTriggered = true;
+                    }
+                    else if (rightHand.x < rightShoulder.x - (triggerDistance) && !moveTriggered)
+                    {
+                        Console.WriteLine("EXEC left");
+                        logic.Move(Direction.Left);
+                        moveTriggered = true;
+                    }
+                    else if (rightHand.y > rightShoulder.y + (triggerDistance / 1.5) && !moveTriggered)
+                    {
+                        Console.WriteLine("EXEC up");
+                        logic.Move(Direction.Up);
+                        moveTriggered = true;
+                    }
+                    else if (rightHand.y < rightShoulder.y - triggerDistance && !moveTriggered)
+                    {
+                        Console.WriteLine("EXEC down");
+                        logic.Move(Direction.Down);
+                        moveTriggered = true;
+                    }
+                    else if (rightHand.z < rightShoulder.z - (triggerDistance * 1.5) && !moveTriggered)
+                    {
+                        Console.WriteLine("EXEC back");
+                        logic.Move(Direction.Back);
+                        moveTriggered = true;
+                    }
+                    else if (rightHand.z > rightShoulder.z - (triggerDistance / 1.5) && !moveTriggered)
+                    {
+                        Console.WriteLine("EXEC fwd");
+                        logic.Move(Direction.Forward);
+                        moveTriggered = true;
+                    }
+                    else if ((rightHand.x < rightShoulder.x + (triggerDistance / 1.9)) &&
+                       (rightHand.x > rightShoulder.x - (triggerDistance / 1.5)) &&
+                        (rightHand.y < rightShoulder.y + (triggerDistance / 1.5)) &&
+                        (rightHand.y > rightShoulder.y - (triggerDistance / 1.5)) &&
+                        (rightHand.z > rightShoulder.z - (triggerDistance * 1.3)) &&
+                        (rightHand.z < rightShoulder.z - (triggerDistance / 1.3)))
+                    {
+                        Console.WriteLine("RESET " + rightHand.x.ToString() + "|" + rightShoulder.x.ToString());
+                        moveTriggered = false;
+                    }
+
+                    return;
                     if (rightHand.x > rightShoulder.x && handShoulderVector.y < handShoulderVector.x && handShoulderVector.Length < triggerDistance && moveRight)
                     {
                         Console.WriteLine("kinect move right exec");
